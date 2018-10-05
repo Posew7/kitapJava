e1eea54ff0fee8e9b2a65f56021960cf97d811b9
posew7 <posew7@gmail.com>
2018-10-05 19:39:27 +0300

    kim korkar javadan while döngüsü örnek

---
+++ src/WhileDongusu.java
@@ -0,0 +1,20 @@
+
+public class WhileDongusu {
+
+    public static void main(String[] args) {
+
+        int sayi = 5;
+
+        while (sayi >= 0) {
+            System.out.println("5 kere döndüreceğiz");
+            sayi--;
+        }
+
+        int abakus = 0;
+        while (abakus <= 10) {
+            System.out.println("abakus = " + abakus);
+            abakus++;
+        }
+        System.out.println("abakus doğru sayıyor");
+    }
+}
