480e4c89db4c2eb9fd23bd652a85b5d3f4e314c5
posew7 <posew7@gmail.com>
2018-10-05 20:01:00 +0300

    kim korkar javadan return deyimi örnek

---
+++ src/ReturnDeyimi.java
@@ -0,0 +1,15 @@
+
+public class ReturnDeyimi {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                return;
+
+            } else {
+                System.out.println("i = " + i);
+            }
+        }
+    }
+}
