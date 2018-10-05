84224a305bddab02dcd40bcc9ad4934e933d3440
posew7 <posew7@gmail.com>
2018-10-05 19:01:47 +0300

    kim korkar javadan if deyimi 1. örnek

---
+++ src/ifDeyimi1.java
@@ -0,0 +1,20 @@
+
+public class ifDeyimi1 {
+
+    public static void main(String[] args) {
+
+        int birinciSayi = 8;
+        int ikinciSayi = 5;
+
+        if (birinciSayi > ikinciSayi) {
+            System.out.println("birinci sayı ikinci sayıdan büyüktür");
+        }
+        if (birinciSayi < ikinciSayi) {
+            System.out.println("birinci sayı ikinci sayıdan küçüktür");
+        }
+        if (birinciSayi != ikinciSayi) {
+            System.out.println("birinci sayı ikinci sayıya eşit değildir");
+        }
+    }
+
+}
