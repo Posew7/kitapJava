931c7628eb62c0507ea7e621cad0936b064dd039
posew7 <posew7@gmail.com>
2018-10-05 19:12:08 +0300

    kim korkar javadan if else deyimi örnek

---
+++ src/IfElseYapisi.java
@@ -0,0 +1,15 @@
+
+public class IfElseYapisi {
+
+    public static void main(String[] args) {
+
+        int birinciSayi = 15;
+        int ikinciSayi = 25;
+
+        if (birinciSayi == ikinciSayi) {
+            System.out.println("birinci sayı ikinci sayıya eşittir");
+        } else {
+            System.out.println("birinci sayı ikinci sayıdan küçüktür");
+        }
+    }
+}
