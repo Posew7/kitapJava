e520b51be2233a84c82c03335af12c7d7a864eb1
posew7 <posew7@gmail.com>
2018-10-06 10:27:02 +0300

    kim korkar javadan 1den 100e, 100den 1e yazdıran abakus örnek

---
+++ src/sayac2.java
@@ -0,0 +1,15 @@
+
+public class sayac2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 100; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+}
