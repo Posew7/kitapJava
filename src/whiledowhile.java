8acbcac48301ecb7c6b9cdee8f4701f54f3e0bb1
posew7 <posew7@gmail.com>
2018-10-06 10:35:47 +0300

    kim korkar javadan while ve do while arasındaki fark

---
+++ src/whiledowhile.java
@@ -0,0 +1,21 @@
+
+public class whiledowhile {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        while (x < 3) {
+            x = x + 1;
+        }
+        System.out.println("x = " + x);
+
+        System.out.println("do while dongusunu inceleyelim");
+
+        int y = 5;
+        do {
+            y = y + 1;
+        } while (y < 3);
+        System.out.println("y = " + y);
+    }
+
+}
