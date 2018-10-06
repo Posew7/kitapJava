c1b6122a62b90cca9b8b88049c8c9ef7948f72c5
posew7 <posew7@gmail.com>
2018-10-06 17:26:43 +0300

    kim korkar javadan diziye eleman olarak gdizi indekslerinin karelerini atayan örnek

---
+++ src/dizi1.java
@@ -0,0 +1,14 @@
+
+public class dizi1 {
+
+    public static void main(String[] args) {
+
+        int dizi[] = new int[9];
+
+        for (int i = 0; i < dizi.length; i++) {
+            dizi[i] = i * i;
+            System.out.println("indis : " + i + " karesi : " + dizi[i]);
+        }
+    }
+
+}
