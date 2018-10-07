54d2bfab7d5f82bb76eaefa15d409bbe61ebf279
posew7 <posew7@gmail.com>
2018-10-07 09:18:21 +0300

    kim korkar javadan parametre olaraak bir dizi elemanını alan bir metot yazalım ve metodu dizi elemanı ile çağıralım

---
+++ src/dizi5.java
@@ -0,0 +1,12 @@
+
+public class dizi5 {
+
+    public static void main(String[] args) {
+        int dizi[] = {3, 6, 2, 8, 7};
+        yazdir(dizi[4]);
+    }
+
+    public static void yazdir(int x) {
+        System.out.println(x);
+    }
+}
