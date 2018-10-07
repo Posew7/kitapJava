05357119fcfe538314af29d6651cb54708dee8db
posew7 <posew7@gmail.com>
2018-10-07 14:37:21 +0300

    kim korkar javadan dizi karşılaştırma örnek

---
+++ src/dizi11.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+
+public class dizi11 {
+
+    public static void main(String[] args) {
+        int dizi1[] = {1, 2, 3, 5, 6};
+        int dizi2[] = {1, 2, 3, 5, 6};
+      
+        System.out.println(Arrays.equals(dizi2, dizi1));
+        
+    }
+
+}
