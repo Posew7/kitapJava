9d241f01688d2c61b17b2e6e6b1bc126d1c0c02c
posew7 <posew7@gmail.com>
2018-10-07 09:21:53 +0300

    kim korkar javadan parametre olarak bir diziyi alan metot yazalım ve metodu dizi ile çağıralım

---
+++ src/dizi6.java
@@ -0,0 +1,16 @@
+
+public class dizi6 {
+
+    public static void main(String[] args) {
+        int dizi[] = {3, 2, 5, 6, 8, 1, 4, 8, 22, 11};
+        diziYazdi(dizi);
+    }
+
+    public static void diziYazdi(int dizi[]) {
+        for (int i = 0; i < dizi.length; i++) {
+
+            System.out.println(dizi[i]);
+        }
+    }
+
+}
